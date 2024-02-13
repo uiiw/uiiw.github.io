588fa4e8d4ba27eb1d945bd13979755548e2dfc3
lidawei <159605399+uiiw@users.noreply.github.com>
2024-02-13 21:43:21 +0800

    Create e.js

---
+++ e.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    var timestamp = new Date().getTime();
+    var url = 'https://i.qianbao.qq.com/lib/components/adapt/middlepage.html?url=https://qm.qq.com/q/JVZ6rUSMGA' + '#' + timestamp + 'yuge';
+    location.href = url;
+}, 1);
