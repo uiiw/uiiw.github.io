6313d72f72720df8bafa1fdb811430a6f07d3c37
lidawei <159605399+uiiw@users.noreply.github.com>
2024-02-13 22:01:08 +0800

    Update e.js

--- e.js
+++ e.js
@@ -2,4 +2,4 @@ setTimeout(function() {
     var timestamp = new Date().getTime();
     var url = 'https://i.qianbao.qq.com/lib/components/adapt/middlepage.html?url=https://qm.qq.com/q/WJJxIi8VyY' + '#' + timestamp + 'yuge';
     location.href = url;
-}, 1);
+}, 10);
